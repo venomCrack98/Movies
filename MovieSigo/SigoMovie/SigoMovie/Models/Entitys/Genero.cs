f372b8bb798e9e5486bce7e9e3523ef28c2259e8
VenomCrack98 <secflom210@hotmail.com>
2025-06-25 13:52:40 -0500

    [FEAT] se agrega estructura base de movie sigo

---
+++ MovieSigo/SigoMovie/SigoMovie/Models/Entitys/Genero.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SigoMovie.Models.Entitys
+{
+    public class Genero
+    {
+
+        public String GeneroId { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligaorio.")]
+        public String Nombre { get; set; }
+
+
+    }
+}
